f61c45427654e4cd81fb67fd62394b6fc0d83993
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-03-15 15:50:27 +0200

    .

--- journal_gl_dashboard_balance/models/account_journal.py
+++ journal_gl_dashboard_balance/models/account_journal.py
@@ -4,11 +4,17 @@ from odoo import models, fields, api
 
 
 class AccountJournal(models.Model):
-    _inherit = 'account.journal'
+    _inherit = "account.journal"
+
+    company_currency_id = fields.Many2one(
+        "res.currency",
+        related="company_id.currency_id",
+        readonly=True
+    )
 
     gl_balance = fields.Monetary(
         string="GL Balance",
-        currency_field='company_currency_id',
+        currency_field="company_currency_id",
         compute="_compute_gl_balance",
         help="Current balance of the journal default account"
     )
@@ -17,41 +23,46 @@ class AccountJournal(models.Model):
         self.ensure_one()
 
         return {
-            'name': 'GL Report',
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move.line',
-            'view_mode': 'tree,form',
-            'domain': [
-                ('account_id', '=', self.default_account_id.id),
-                ('parent_state', '=', 'posted')
+            "name": "GL Report",
+            "type": "ir.actions.act_window",
+            "res_model": "account.move.line",
+            "view_mode": "tree,form",
+            "domain": [
+                ("account_id", "=", self.default_account_id.id),
+                ("parent_state", "=", "posted"),
+                ("company_id", "=", self.company_id.id),
             ],
-            'context': {
-                'create': False
-            }
+            "context": {
+                "create": False
+            },
         }
 
-    @api.depends('default_account_id')
+    @api.depends("default_account_id")
     def _compute_gl_balance(self):
 
         journals = self.filtered(lambda j: j.default_account_id)
-
         balances = {}
 
         if journals:
-            account_ids = journals.mapped('default_account_id').ids
+
+            account_ids = journals.mapped("default_account_id").ids
+            company_ids = journals.mapped("company_id").ids
 
             query = """
-                SELECT account_id, SUM(debit - credit)
+                SELECT account_id, company_id, SUM(debit - credit) AS balance
                 FROM account_move_line
                 WHERE account_id IN %s
+                AND company_id IN %s
                 AND parent_state = 'posted'
-                GROUP BY account_id
+                GROUP BY account_id, company_id
             """
 
-            self.env.cr.execute(query, (tuple(account_ids),))
+            self.env.cr.execute(query, (tuple(account_ids), tuple(company_ids)))
             results = self.env.cr.fetchall()
 
-            balances = {acc_id: balance for acc_id, balance in results}
+            for account_id, company_id, balance in results:
+                balances[(account_id, company_id)] = balance
 
         for journal in self:
-            journal.gl_balance = balances.get(journal.default_account_id.id, 0.0)
+            key = (journal.default_account_id.id, journal.company_id.id)
+            journal.gl_balance = balances.get(key, 0.0)
